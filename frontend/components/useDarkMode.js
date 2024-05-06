158621d4f7f20ffcac50d82100cbd078eb093b21
Michelle Staton <mstaton618@gmail.com>
2024-05-06 13:36:17 -0400

    work in progress

---
+++ frontend/components/useDarkMode.js
@@ -0,0 +1,6 @@
+import { useLocalStorage } from '../components/hooks/useLocalStorage.js'
+
+export function useDarkMode() {
+    const {dark, setDark} = useLocalStorage('darkmode', false)
+    return {dark, setDark}
+}
