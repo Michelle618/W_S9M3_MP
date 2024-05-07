5adc7f8eb95c41cdc58481a83e7763bb324f740e
Michelle Staton <mstaton618@gmail.com>
2024-05-07 11:12:18 -0400

    project complete

--- frontend/components/useDarkMode.js
+++ frontend/components/useDarkMode.js
@@ -1,6 +1,6 @@
 import { useLocalStorage } from '../components/hooks/useLocalStorage.js'
 
 export function useDarkMode() {
-    const {dark, setDark} = useLocalStorage('darkmode', false)
-    return {dark, setDark}
+    const [dark, setDark] = useLocalStorage('darkmode', false)
+    return [dark, setDark]
 }
